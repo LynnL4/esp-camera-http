3cbec788d4341f0e336e27ef3807bf891be45e7f
LynnL4 <lht856@foxmail.com>
2025-02-07 11:47:46 +0800

    Init

---
+++ main/main.c
@@ -0,0 +1,224 @@
+#include <esp_log.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include <string.h>
+#include <driver/i2c.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+
+#include "esp_camera.h"
+#include "esp_http_server.h"
+#include "esp_timer.h"
+
+#include "iperf_cmd.h"
+
+#include "console_wifi.h"
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+#define TAG "main"
+
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req)
+{
+    camera_fb_t *fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t *_jpg_buf;
+    char *part_buf[64];
+    static int64_t last_frame = 0;
+    if (!last_frame)
+    {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if (res != ESP_OK)
+    {
+        return res;
+    }
+
+    while (true)
+    {
+        fb = esp_camera_fb_get();
+        if (!fb)
+        {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if (fb->format != PIXFORMAT_JPEG)
+        {
+            ESP_LOGI(TAG, "Converting JPEG...");
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if (!jpeg_converted)
+            {
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        }
+        else
+        {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if (res == ESP_OK)
+        {
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if (fb->format != PIXFORMAT_JPEG)
+        {
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        if (res != ESP_OK)
+        {
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        ESP_LOGI(TAG, "MJPG: %luKB %lums (%.1ffps)",
+                 (uint32_t)(_jpg_buf_len / 1024),
+                 (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
+}
+
+#define CAM_PIN_PWDN -1
+#define CAM_PIN_RESET -1
+
+#define CAM_PIN_VSYNC 38
+#define CAM_PIN_HREF 47
+#define CAM_PIN_PCLK 13
+#define CAM_PIN_XCLK 10
+
+#define CAM_PIN_SIOD 40
+#define CAM_PIN_SIOC 39
+
+#define CAM_PIN_D0 15
+#define CAM_PIN_D1 17
+#define CAM_PIN_D2 18
+#define CAM_PIN_D3 16
+#define CAM_PIN_D4 14
+#define CAM_PIN_D5 12
+#define CAM_PIN_D6 11
+#define CAM_PIN_D7 48
+
+static camera_config_t camera_config = {
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sccb_sda = CAM_PIN_SIOD,
+    .pin_sccb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    .xclk_freq_hz = 6000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_GRAYSCALE, // YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_VGA,      // QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+
+    .jpeg_quality = 12, // 0-63, for OV series camera sensors, lower number means higher quality
+    .fb_count = 2,      // When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .grab_mode = CAMERA_FB_IN_PSRAM,
+};
+
+void app_main(void)
+{
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_err_t ret = nvs_flash_init(); // Initialize NVS
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(esp_camera_init(&camera_config));
+
+    sensor_t *sensor = esp_camera_sensor_get();
+    //sensor->set_vflip(sensor, 1);
+    sensor->set_hmirror(sensor, 1);
+
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    /* Use the URI wildcard matching function in order to
+     * allow the same handler to respond to multiple different
+     * target URIs which match the wildcard scheme */
+    config.uri_match_fn = httpd_uri_match_wildcard;
+
+    ESP_LOGI(TAG, "Starting HTTP Server on port: '%d'", config.server_port);
+    ESP_ERROR_CHECK(httpd_start(&server, &config));
+
+    /* URI handler for getting uploaded files */
+    httpd_uri_t stream = {
+        .uri = "/stream", // Match all URIs of type /path/to/file
+        .method = HTTP_GET,
+        .handler = jpg_stream_httpd_handler,
+        .user_ctx = NULL // Pass server data as context
+    };
+
+    httpd_register_uri_handler(server, &stream);
+    uint32_t frame_time = 0;
+
+    // while (true)
+    // {
+    //     camera_fb_t *fb = esp_camera_fb_get();
+    //     if (!fb)
+    //     {
+    //         ESP_LOGE(TAG, "Camera capture failed");
+    //         return;
+    //     }
+    //     esp_camera_fb_return(fb);
+    //     ESP_LOGI(TAG, "Camera capture took %llu ms %llu fps", (esp_timer_get_time() - frame_time) / 1000, 1000000 / (esp_timer_get_time() - frame_time));
+    //     frame_time = esp_timer_get_time();
+    // }
+
+    // Initialize console REPL
+    ESP_ERROR_CHECK(console_cmd_init());
+
+    ESP_ERROR_CHECK(console_cmd_wifi_register());
+
+    // start console REPL
+    ESP_ERROR_CHECK(console_cmd_start());
+}
